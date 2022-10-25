477e8d101c71487230aa0096a9a677d587b3c9ed
sublime-wax <116157983+sublime-wax@users.noreply.github.com>
2022-10-25 14:24:27 +0200

    Update index.js
    test

--- pages/index.js
+++ pages/index.js
@@ -22,23 +22,6 @@ export default function Home() {
          <p className="description">
           Decouvrez sans plus tarder dans notre boutique:
          </p>
-       
-        <p className="description">nos collections de <a href="https://www.sublime-wax.com/collections/robes" target="_blank">robes africaines et robes en wax</a> pour toutes occassions</p>
-        <p className="description">de magnifiques <a href="https://www.sublime-wax.com/collections/woman-african-skirts">jupes africaines et jupe en wax</a> pour vous sublimer</p>
-        <p className="description">
-          Notre collection de  <a href="https://www.sublime-wax.com/collections/accessories">bijoux africain</a>, des produits originaux comme nos <a href="https://www.sublime-wax.com/collections/accessories/products/sublimewax-leather-bag-and-bogolan-gana">sacs en cuir, bogolan ou en wax </a>
-        <p className="description">
-          Fait mains par des artisans Kenya <a href="https://www.sublime-wax.com/collections/massai-pearl-earrings">Des bijoux en perles Massai</a>
-        </p>
-        <p className="description">
-          Nous proposons egalement  <a href="https://www.sublime-wax.com/collections/tenue-africaine-pour-couple-sublimewax">des tenues africaines pour couples</a> pour plus de comlicités pour vos ceremonies.
-        </p>
-    
-        <a href="https://https://twitter.com/sublimewax" 
-          class="twitter-follow-button" 
-          data-show-count="false">Follow @sublimewax
-        </a>
-          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     
       </main>
 
